33f79dab0598e6992b798e9a9b344cb15a84e500
Angurii <139727215+Angurii@users.noreply.github.com>
2024-04-29 13:09:05 +0545

    Commit

---
+++ Common/Middleware/DTOs/ResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace ApplicationDev.Common.Middleware.DTOs
+{
+	public record ResponseDTO
+	{
+		public DateTime Date { get; set; }
+		public required string RequestedUrl { get; set; }
+
+		public string Message { get; set; }
+		public int StatusCode { get; set; }
+
+		public required object Data { get; set; }
+
+	}
+}
