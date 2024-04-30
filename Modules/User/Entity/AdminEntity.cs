fe7f469b1a30f3698a8961c82b6ba8b514197fb0
Angurii <139727215+Angurii@users.noreply.github.com>
2024-04-30 20:02:42 +0545

    Admin Entity

---
+++ Modules/User/Entity/AdminEntity.cs
@@ -0,0 +1,10 @@
+﻿using ApplicationDev.Common.Database.BaseEntity;
+
+namespace ApplicationDev.Modules.Admin.Entity
+{
+	public class AdminEntity : BaseUserEntity
+	{
+		public required string Name { get; set; }
+
+	}
+}
